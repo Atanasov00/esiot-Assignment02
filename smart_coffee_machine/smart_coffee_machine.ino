1c22f116f29691c2aedded52a71ae480c09f5014
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-04-27 14:08:28 +0200

    with scheduler implementation

--- smart_coffee_machine/smart_coffee_machine.ino
+++ smart_coffee_machine/smart_coffee_machine.ino
@@ -1,16 +1,13 @@
-#include <LiquidCrystal_I2C.h> 
+#include "Scheduler.h"
 
-/* Wiring: SDA => A4, SCL => A5 */
-/* I2C address of the LCD: 0x27 */
-/* Number of columns: 20 rows: 4 */
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,20,4); 
+Scheduler scheduler;
 
 void setup() {
-  lcd.init();
-  lcd.backlight();
+  scheduler.init(50);
+
+  //Add tasks here
 }
 
 void loop() {
-  lcd.setCursor(2, 1); // Set the cursor on the third column and first row.
-  lcd.print("Hello World!");
+  scheduler.schedule();
 }
