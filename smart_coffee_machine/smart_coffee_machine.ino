e91f040af5bfb93de13b575ef6427102b55edc99
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-05-01 15:21:13 +0200

    add task

--- smart_coffee_machine/smart_coffee_machine.ino
+++ smart_coffee_machine/smart_coffee_machine.ino
@@ -1,11 +1,17 @@
 #include "Scheduler.h"
+#include "Display.h"
+#include "BootTask.h"
 
 Scheduler scheduler;
 
 void setup() {
   scheduler.init(50);
 
+  Display* lcd = new Display();
+  Task* bootTask = new BootTask(lcd);
+
   //Add tasks here
+  scheduler.addTask(bootTask);
 }
 
 void loop() {
