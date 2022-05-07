08c5206e6775f378cd6e5adb0e0f95bbf7c9242d
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-05-07 15:38:56 +0200

    update

--- smart_coffee_machine/SelfCheckTask.cpp
+++ smart_coffee_machine/SelfCheckTask.cpp
@@ -17,7 +17,6 @@ SelfCheckTask::SelfCheckTask(Display* plcd): lcd(plcd) {
 }
 
 void SelfCheckTask::init(){
-  
   temp = new TempSensorTMP36(TEMP_PIN);
   pos = 0;
   delta = 1;
@@ -29,35 +28,37 @@ void SelfCheckTask::tick(){
       Serial.println("SelfCheckTask");
       startTime = millis();
       state = WAITING;
-      servo->on();
     }
     break;
     case WAITING: {
      //Serial.println("SelfCheckTask2");
      actualTime = millis();
-     if(actualTime - startTime >= 10000){
+     if(actualTime - startTime >= T_CHECK){
       lcd->getLcd().clear();
       lcd->print("Doing self test", 2, 1);
-      servo->setPosition(0);
-      delay(500);
+      selectionTask->setActive(false);
+      userPresenceTask->setActive(false);
       startTime = millis();
+      servo->on();
       state = CHECKING;
      }
-     servo->off();
     }
     break;
     case CHECKING: {
       actualTime = millis();
       if(actualTime - startTime <= 10000){
-        servo->on();
-        for(int i = 0; i < 180; i++){
+        while(pos < 180){
           servo->setPosition(pos);
           pos+=delta;
         }
-        servo->off();
         pos -= delta;
         delta = -delta;
         delay(1000);
+      } else {
+        servo->off();
+        selectionTask->setActive(true);
+        userPresenceTask->setActive(true);
+        state = INIT;
       }
     }
     break;
